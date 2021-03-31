70f8b4fd58fb75a3041fb6366f2db25453b49767
Cal Withington <scwithington@gmail.com>
2021-03-30 23:58:46 -0400

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # super-duper-palm-tree
+https://scwithington.github.io/super-duper-palm-tree/
+## Description
+This is my weather dashboard.
+[Screenshot](./super-duper-palm-tree/assets/images/screenshot.jpg)
+## Deployed Site
+This is the deployed webpage.
+[Deployed Site](https://scwithington.github.io/super-duper-palm-tree/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 const forecastLength = 5;
-var searchHistory = JSON.parse(localStorage.getItem('weatherHistory'))||[]; // will load search history if any, otherwise empty array
-const apiKey = '03980b537ca60f266b837f5334942ed2';
+var searchHistory = JSON.parse(localStorage.getItem("weatherHistory")) || []; // will load search history if any, otherwise empty array
+const apiKey = "03980b537ca60f266b837f5334942ed2";
 
-var userInput = $('#cityInput');
+var userInput = $("#cityInput");
 var cityName;
 
 var weatherHeading;
@@ -10,144 +10,127 @@ var temperature;
 var humidity;
 var wind;
 
-$('#refresh-btn').on('click', function() {
-    localStorage.removeItem('weatherHistory');
-    location.reload();
+$("#refresh-btn").on("click", function () {
+  localStorage.removeItem("weatherHistory");
+  location.reload();
 });
 
-$('#submit-btn').on('click', function() {
-    if (userInput.val() === null) {
-        return;
-    } else {
-        cityName = userInput.val();
-        createHistoryItem(cityName);
-        displayWeather(cityName);
-    } 
+$("#submit-btn").on("click", function () {
+  if (userInput.val() === null) {
+    return;
+  } else {
+    cityName = userInput.val();
+    createHistoryItem(cityName);
+  }
+  displayWeather(cityName);
 });
 
 displayHistory();
 
 function displayWeather(cityName) {
-
-    var forecastApi = 
-    'https://api.openweathermap.org/data/2.5/forecast?q=' +
+  var forecastApi =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
     cityName +
-    "&units=imperial&appid=" + 
+    "&units=imperial&appid=" +
     apiKey;
 
-    var weatherApi = 
-    'https://api.openweathermap.org/data/2.5/weather?q=' +
+  var weatherApi =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
     cityName +
-    '&units=imperial&appid=' + 
+    "&units=imperial&appid=" +
     apiKey;
 
-    $.ajax({
-        url: weatherApi,
-        method: 'GET'
-    }).then(function(response) {        
-        console.log(response);
-        getUVIndex(response.coord.lat, response.coord.lon);
-        
-        // show name of city
-        weatherHeading = $('<h4>').text(cityName);
-        var myCityTitle = $('#cityTitle');
-        myCityTitle.append(weatherHeading);
+  $.ajax({
+    url: weatherApi,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    getUVIndex(response.coord.lat, response.coord.lon);
 
-        // Temperature of 'city'
-        temperature = $('<p>').text(
-            'Temperature: ' + Math.round(response.main.temp) + '°F'
-        );
-        // Humidity
-        humidity = $('<p>').text(
-            'Humidity: ' + response.main.humidity + '%'
-        );
-        // Wind
-        wind = $('<p>').text(
-            'Wind Speed: ' + Math.round(response.wind.speed) + 'mph'
-        )
-        
-        //Append to html
-        $('#weatherForCity').append(temperature, humidity, wind);
+    // show name of city
+    weatherHeading = $("<h4>").text(cityName);
+    var myCityTitle = $("#cityTitle");
+    myCityTitle.append(weatherHeading);
 
+    // Temperature of 'city'
+    temperature = $("<p>").text(
+      "Temperature: " + Math.round(response.main.temp) + "°F"
+    );
+    // Humidity
+    humidity = $("<p>").text("Humidity: " + response.main.humidity + "%");
+    // Wind
+    wind = $("<p>").text(
+      "Wind Speed: " + Math.round(response.wind.speed) + "mph"
+    );
 
-        
-    });
+    //Append to html
+    $("#weatherForCity").append(temperature, humidity, wind);
+  });
 
-    $.ajax({
-        url: forecastApi,
-        method: 'GET'
-    }).then(function(response) {
-        $('#forecastName').text('5 Day Forecast');
-        for(var i = 1; i < forecastLength; i++) {
-            var card = $("<div class='forecastCard'>");
-            
-            // unix timestamp 
-            var sec = response.daily[i];
-            var mS = sec * 1000;
-            var date = new Date(mS);
-            var dayOfWeek = date.toLocaleString('en-US', { weekday: 'long'});
+  $.ajax({
+    url: forecastApi,
+    method: "GET",
+  }).then(function (response) {
+    $("#forecastName").text("5 Day Forecast");
+    for (var i = 1; i < forecastLength; i++) {
+      var card = $("<div class='forecastCard'>");
 
-            var fCardHeader = $('h4');
-            fCardHeader.text(dayOfWeek);
-            
-            // Getting icon and appending
-            var icon = $('<img>');
-            icon.attr(
-                'src', "http://openweathermap.org/img/wn/" +
-                response.daily[i].weather[0].icon +
-                ".png"
-            );
-            var iconCase = ('<p>').text='please show up';
-            
-            iconCase.append(icon);
+      // unix timestamp
+      var sec = response.daily[i];
+      var mS = sec * 1000;
+      var date = new Date(mS);
+      var dayOfWeek = date.toLocaleString("en-US", { weekday: "long" });
 
-            var cardTemp = $('<p>').text(
-                'Temp:' + Math.round(response.daily[i].temp.day)
-            );
-            
-            
-            
-            forecastCard.append (
-                fCardHeader, iconCase, cardTemp
-            );
-        
-            $('#forecast').append(card);
-        }
+      var fCardHeader = $("h4");
+      fCardHeader.text(dayOfWeek);
 
-    });
+      // Getting icon and appending
+      var icon = $("<img>");
+      icon.attr(
+        "src",
+        "http://openweathermap.org/img/wn/" +
+          response.daily[i].weather[0].icon +
+          ".png"
+      );
+      var iconCase = ("<p>".text = "");
 
-    // Append City Name to HTML
+      iconCase.append(icon);
 
+      var cardTemp = $("<p>").text("Temp:" + Math.round(sec.temperature.day));
 
+      card.append(fCardHeader, iconCase, cardTemp);
 
-    
+      $("#forecast").append(card);
+    }
+  });
+
+  // Append City Name to HTML
 }
 
-function createHistoryItem (cityName) {
-    if(searchHistory.indexOf(cityName) === -1) {
-        searchHistory.push(cityName);
+function createHistoryItem(cityName) {
+  if (searchHistory.indexOf(cityName) === -1) {
+    searchHistory.push(cityName);
 
-        localStorage.setItem('weatherHistory', JSON.stringify(searchHistory));
-        displayHistory();
-    }
+    localStorage.setItem("weatherHistory", JSON.stringify(searchHistory));
+    displayHistory();
+  }
 }
 
 function displayHistory() {
-       var listEl = $('#search-history');
-    listEl.html('');
-    for(var i = 0; i < searchHistory.length; i++) {
-        var li = $('<li>');
+  var listEl = $("#search-history");
+  listEl.html("");
+  for (var i = 0; i < searchHistory.length; i++) {
+    var li = $("<li>");
 
-        li.text(searchHistory[i]);
-        li.on('click', function(event){
-            var city = event.target.innerText;
-            displayWeather(city);
-        })
-        listEl.append(li);
-    }
+    li.text(searchHistory[i]);
+    li.on("click", function (event) {
+      var city = event.target.innerText;
+      displayWeather(city);
+    });
+    listEl.append(li);
+  }
 }
 
-function getUVIndex (lat, long) {
-    console.log(lat, long);
-
+function getUVIndex(lat, long) {
+  console.log(lat, long);
 }
